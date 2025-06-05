9386c9006610a1014c88ed70ff784373fac6c15a
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-06-05 15:37:07 +0200

    Add light bulb functionality with toggle button and styling

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("Prova");
+const toggleBtn = document.getElementById("lightToggleBtn");
+const lightImg = document.querySelector(".light-bulb");
+
+toggleBtn.addEventListener('click', function () {
+    lightImg.src = "./img/yellow_lamp.png";
+})
